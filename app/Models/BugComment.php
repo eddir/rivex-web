1241fb6a9dde9d6ab0ac9aba29768715319b58d3
Eduard Rostkov <eddirworkmail@gmail.com>
2019-01-20 14:38:05 +0100

    Модель комментария бага
     necessary,

---
+++ app/Models/BugComment.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BugComment extends Model
+{
+  /**
+  * The attributes that are mass assignable.
+  *
+  * @var array
+  */
+  protected $fillable = [
+      'body'
+  ];
+
+  /**
+   * Many to Many relation
+   *
+   * @return \Illuminate\Database\Eloquent\Relations\belongsToMany
+   */
+  public function bug()
+  {
+    return $this->belongsTo(Bug::class);
+  }
+
+  /**
+   * Many to Many relation
+   *
+   * @return \Illuminate\Database\Eloquent\Relations\belongsToMany
+   */
+  public function user()
+  {
+    return $this->belongsTo(User::class);
+  }
+}
