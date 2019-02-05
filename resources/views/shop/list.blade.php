823c6f9c7ac222c72d4e1d7ded29cdedf8b67ed0
Eduard Rostkov <eddirworkmail@gmail.com>
2019-02-05 11:55:24 +0100

    Revert "Перевод для руб./мес."
    This reverts commit 28edc76b3df5f546ab04073b28e0315f4d8ed60d.

--- resources/views/shop/list.blade.php
+++ resources/views/shop/list.blade.php
@@ -5,7 +5,7 @@
     <h1>@lang('List of privilages')</h1>
 
     @foreach ($products as $product)
-        <h2>{{ $product->title }} <i>({{ $product->price }} @lang('Rub per month'))</i></h2>
+        <h2>{{ $product->title }} <i>({{ $product->price }} @lang('rub'))</i></h2>
         {!! $product->body !!}
     @endforeach
 
